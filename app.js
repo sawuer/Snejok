d73bc06129666bdbe39e5113b39cd1ae7a6d0660
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-08-29 18:46:20 +0600

    Init

---
+++ app.js
@@ -0,0 +1,6 @@
+new Templa('#temp1', {
+	title: 'Hello world',
+	title2: 'Hello world22222',
+	someText: 'Some Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, iusto.',
+	text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ipsam!'
+});
