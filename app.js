99c3902c7e8453d4f8853f3e0365c6af172cfb5a
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-08-30 02:13:44 +0600

    If statement start

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ new Snegyok('#temp1', {
 	title: 'Hello world',
 	text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ipsam!',
 	list2: ['somelist1', 'dsfsdfsd', 'somelist2', 'somelist3', 'somelist4','somelist6', 'somelist2', 'somelist3' ],
-	some: 'FUUUCCII'
+	some: 'FUUUCCII',
+	bool: true,
+	bool2: true,
 });
--- snegyok.js
+++ snegyok.js
@@ -10,14 +10,16 @@ this.Snegyok = (function() {
 		// Public methods
 		this.entry = entry;
 		this.data = confs;
-
 		
+		// Private data
+		var view = document.querySelector(this.entry).innerHTML;
+
 		/* * * * * * * Templater * * * * * * */
 
 		function templater(entry, data) {
-			var dataArray = Object.keys(data);
-			var entry = document.querySelector(entry);
-			var view = entry.innerHTML;
+			var 
+				dataArray = Object.keys(data),
+				entry = document.querySelector(entry);
 			function key(index) {
 				return Object.keys(data)[index];
 			}
@@ -25,28 +27,30 @@ this.Snegyok = (function() {
 				var pat = new RegExp('\\(\\* ' + key(i) + ' \\*\\)', 'g')
 			  view = view.replace(pat, data[key(i)]);
 			}
-			entry.innerHTML = view;
 		}
 
+		function booler(entry, data) {
+			
+		}
 
 		/* * * * * * * * *
 		* * * Looper * * *
 		 * * * * * * * */
 
 		function looper(entry, data) {
-			var dataArray = Object.keys(data);
-			var entry = document.querySelector(entry);
-			var view = entry.innerHTML;
-			var pat = /\(\* for[\s\S]*?endfor \*\)/gm;
+			var 
+				dataArray = Object.keys(data);
+				entry = document.querySelector(entry),
+				pat = /\(\* for[\s\S]*?endfor \*\)/gm,
+				splittedArrays = [],
+				allLoopsArray = view.match(pat),
 
-			var allLoopsArray = view.match(pat);
-			var removeEnds = allLoopsArray.map(function(i) {
-				return i
-					.replace(['(* endfor *)'], '')
-					.replace('(* for', '');
-			});
+				removeEnds = allLoopsArray.map(function(i) {
+					return i
+						.replace(['(* endfor *)'], '')
+						.replace('(* for', '');
+				});
 
-			var splittedArrays = [];
 
 			for (var i = 0; i < removeEnds.length; i++) {
 				var newArr = [];
@@ -55,10 +59,13 @@ this.Snegyok = (function() {
 			}
 
 			for (var i = 0; i < splittedArrays.length; i++) {
-				var currentList = splittedArrays[i][0][0].replace(/\s*/g,'');
-				var currentHTML = splittedArrays[i][0][1];
-				var inpat = new RegExp('\\(\\* for ' + currentList + ' [\\s\\S]*?endfor \\*\\)', 'g')
-				var newHTML = '';
+				var 
+					currentList = splittedArrays[i][0][0].replace(/\s*/g,''),
+					currentHTML = splittedArrays[i][0][1],
+					inpat = new RegExp(
+						'\\(\\* for ' + currentList + ' [\\s\\S]*?endfor \\*\\)', 'g'
+					),
+					newHTML = '';
 
 				for (var j = 0; j < self.data[currentList].length; j++) {
 					var currentItemInList = self.data[currentList][j];
@@ -72,9 +79,10 @@ this.Snegyok = (function() {
 		}
 
 
+
+		booler(this.entry, this.data);
 		templater(this.entry, this.data);
 		looper(this.entry, this.data);
-
 
 	}
 
