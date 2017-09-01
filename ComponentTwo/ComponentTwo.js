3df399adf4d09647f754beddf720d4203cf79941
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-09-02 04:28:17 +0600

    Separate on modules

---
+++ ComponentTwo/ComponentTwo.js
@@ -0,0 +1,5 @@
+var ComponentTwo = new Snegyok('ComponentTwo', {
+	title: 'Component Two',
+	text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, ipsam!',
+	music: ['some1', 'some2'],
+});
